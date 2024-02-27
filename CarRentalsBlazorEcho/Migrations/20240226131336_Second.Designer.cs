aba696b20c82858127b3ea4c5ab7aafd302f3b74
gustaf frejd <gustaf.frejd@hotmail.com>
2024-02-27 08:11:42 +0100

    edit

--- CarRentalsBlazorEcho/Migrations/20240226131336_Second.Designer.cs
+++ CarRentalsBlazorEcho/Migrations/20240226131336_Second.Designer.cs
@@ -3,6 +3,7 @@ using CarRentalsBlazorEcho.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarRentalsBlazorEcho.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240226131336_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
