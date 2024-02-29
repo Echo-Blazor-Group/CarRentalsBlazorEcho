fa5a5a7b1e8975dbbc86b40dbbaf42c9be80f138
Nujsk <111609565+Nujsk@users.noreply.github.com>
2024-02-29 13:28:26 +0100

    Test

--- CarRentalsBlazorEcho/Data/ApplicationDbContext.cs
+++ CarRentalsBlazorEcho/Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace CarRentalsBlazorEcho.Data
     public class ApplicationDbContext:DbContext
     {
         public DbSet<Car> Cars { get; set; }
-        public DbSet<Admin> Admins { get; set; }
         public DbSet<CarCategory> CarCategories { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet <User> Users { get; set; }
